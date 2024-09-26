78175a93630c6f255ad26faeb5cc533cf2fbd593
Pol-ly <Sal_pol@mail.ru>
2024-09-26 21:50:56 +0300

    Add files via upload

---
+++ info/3 адание Скрыпникова (1 вар).py
@@ -0,0 +1,5 @@
+a = float (int (input ('Введите число ')))
+b = float (int (input ('Введите число ')))
+c = float (int (input ('ведите число ')))
+sum = a + b + c
+print ('Сумма трёх чисел равна: ', sum)
