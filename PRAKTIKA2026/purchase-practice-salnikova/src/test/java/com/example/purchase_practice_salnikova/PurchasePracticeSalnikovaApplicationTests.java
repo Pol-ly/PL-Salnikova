cc9ed00de5310ee4d71ac1fdc451fe0f5bde089b
Pol-ly <Sal_pol@mail.ru>
2026-01-28 13:00:02 +0300

    Add files via upload

---
+++ PRAKTIKA2026/purchase-practice-salnikova/src/test/java/com/example/purchase_practice_salnikova/PurchasePracticeSalnikovaApplicationTests.java
@@ -0,0 +1,12 @@
+package com.example.purchase_practice_salnikova;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PurchasePracticeSalnikovaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
